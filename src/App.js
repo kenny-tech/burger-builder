76d39c249ce5b33a748e0dead96acb69876c73d8
Kehinde Adegbesan <kenny@Kehindes-MacBook-Pro.local>
2018-06-05 12:06:50 +0100

    Create burger builder website layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Welcome to React!</p>
+          <BurgerBuilder/>
         </Layout>
       </div>
     );
