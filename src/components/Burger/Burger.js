43bf462bfbc753de1e95635c17e877d130d7582b
kenny-tech <kenny4real2001@gmail.com>
2018-07-25 11:16:53 +0100

    Add buttons for adding and removing burger ingriedients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
         //Transform the ingredients object into an array
-        const transformedIngredients = Object.keys( props.ingredients )
+        let transformedIngredients = Object.keys( props.ingredients )
         //map through the new array string
         .map( igKey => {
             return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients</p>;
+        }
+        
         return(
             <div className={classes.Burger}>
               <BurgerIngredient type="bread-top" />
