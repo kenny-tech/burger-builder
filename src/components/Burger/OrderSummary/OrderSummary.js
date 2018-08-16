97aa86857bae767fbd94be27cbc4be037c44c3d8
kenny-tech <kenny4real2001@gmail.com>
2018-08-16 23:50:50 +0100

    Added Component Lifecycle to Modal Component so that OrderSummary Component is not unnecessary updated or rerendered

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-        return (
-            <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li> );
-    });
-  return (
-    <Aux>
-        <h3>Your Order</h3>
-        <p>A delicous burger with the following ingredients:</p>
-        <ul>
-            {ingredientSummary}
-        </ul>
-        <p><strong>Total Price: {props.price}</strong></p>
-        <p>Continue to Checkout?</p>
-        <Button btyType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-        <Button btyType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
+
+class OrderSummary extends Component {
+    componentWillUpdate() {
+      console.log('[OrderSummary] WillUpdate');
+    }
+
+    render () {
+      const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li> );
+        });
+
+      return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicous burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: {this.props.price}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btyType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btyType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </Aux>
+      );
+    }
+}
 
 export default OrderSummary;
