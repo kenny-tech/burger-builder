c494e204f22e911e5d144db4cc4dc53b49f37c6c
kenny-tech <kenny4real2001@gmail.com>
2019-01-02 14:33:47 +0100

    Display spinner when submitting order to the server

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -10,7 +10,7 @@ class Modal extends Component {
   // OrderSummary is not unnecessary updated or rendered
   // This improves the app performance
   shouldComponentUpdate(nextProps, nextState) {
-      return nextProps.show !== this.props.show;
+      return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
   componentWillUpdate() {
